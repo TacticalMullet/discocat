fa6a913945aa3cdf4af8e03a1f50cf0caee26430
mert <33905560+aridmeme@users.noreply.github.com>
2021-08-29 09:53:43 -0600

    add progress reports

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
+import khttp.extensions.fileLike
+import org.json.JSONObject
 import java.io.BufferedReader
+import java.io.File
 import java.io.InputStreamReader
+import java.util.*
 
 
 fun main() {
@@ -14,16 +18,34 @@ fun main() {
 
 // local configuration
 const val LOGCAT_CMD = "-s 127.0.0.1:5557 logcat"
-const val CLEAR_CMD = "logcat -c"
+const val CLEAR_CMD = "-s 127.0.0.1:5557 logcat -c"
 
 const val PREFIX = "DISCORD_69_69_420" // prefix the messages you want to grep for with this
+const val PROG_PREFIX = "DISCORD_69_69_PROG" // prefix the messages you want to grep for with this
 
-val HEADERS = mapOf("Accept" to "application/json", "Content-Type" to "application/json")
+val JSON_HEADERS = mapOf("Accept" to "application/json", "Content-Type" to "application/json")
+val FILE_HEADERS = mapOf("Content-Type" to "multipart/form-data")
 
-class DiscordLogcat(var adbPath: String = "", var webhook: String = "") {
+class DiscordLogcat(
+    var adbPath: String = "",
+    var webhook: String = "",
+    var webhookProg: String = "",
+    var imgGen: String = "",
+) {
     init {
         adbPath = this::class.java.getResource("adb.properties").readText() // location of adb.exe
         webhook = this::class.java.getResource("discord.webhook").readText() // discord webhook url
+        webhookProg = this::class.java.getResource("discord.webhook.prog").readText() // discord webhook url
+        imgGen = this::class.java.getResource("img.gen").readText() // dank image generator
+    }
+
+    fun getImage(payload: JSONObject): File {
+        val img = khttp.post(imgGen, json = payload, headers = JSON_HEADERS)
+        val base64Img = img.jsonObject.getString("body")
+            .replace("data:image/png;base64,", "")
+            .replace("\"", "")
+
+        return File("temp.png").apply { writeBytes(Base64.getDecoder().decode(base64Img)) }
     }
 
     fun discoCat() {
@@ -38,15 +60,27 @@ class DiscordLogcat(var adbPath: String = "", var webhook: String = "") {
         while (true) {
             inputStreamReader
                 .readLine()
-                .takeIf { it.contains(PREFIX) }
-                ?.sanitize()
-                ?.also { msg ->  println(msg) }
-                ?.post()
-                ?.also { response -> println(response) }
+                .also {
+                    when {
+                        it.contains(PREFIX) -> {
+                            it.sanitize()
+                                .also { println(it) }
+                                .postToWebhook()
+                        }
+                        it.contains(PROG_PREFIX) -> {
+                            it.sanitize()
+                                .also { println(it) }
+                                .let { getImage(JSONObject(it)) }
+                                .postToWebhook(webhookProg)
+                        }
+                        else -> {}
+                    }
+                }
         }
     }
 
-    fun String.post() = khttp.post(webhook, data = mapOf("content" to this), headers = HEADERS)
+    fun String.postToWebhook(wh: String = webhook) = khttp.post(wh, data = mapOf("content" to this), headers = JSON_HEADERS)
+    fun File.postToWebhook(wh: String = webhook) = khttp.post(wh, files = listOf(this.fileLike()), headers = FILE_HEADERS)
 }
 
 private fun String.sanitize() =
@@ -54,3 +88,9 @@ private fun String.sanitize() =
         .replace("����", " ") // this replaces some weird unicode space
         .replaceBefore(PREFIX, "")
         .replace(PREFIX, "")
+        .replaceBefore(PROG_PREFIX, "")
+        .replace(PROG_PREFIX, "")
+
+
+
+
