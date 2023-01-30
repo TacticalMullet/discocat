66c3382a1a7f5d27aa01a39911d3bbcfa5e5c7bf
Mert <33905560+TacticalMullet@users.noreply.github.com>
2023-01-29 20:05:18 -0700

    collection logs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -19,6 +19,7 @@ const val CLEAR_CMD = "-s 127.0.0.1:5555 logcat -c"
 const val PREFIX = "DISCORD_69_69_420" // prefix the messages you want to grep for with this
 const val PROG_PREFIX = "DISCORD_69_69_PROG" // prefix the messages you want to grep for with this
 const val SPAM_PREFIX = "DISCORD_69_69_SPAM" // prefix the messages you want to grep for with this
+const val COLLECTION_LOG_PREFIX = "DISCORD_69_69_COLLECTION_LOG" // prefix the messages you want to grep for with this
 
 val JSON_HEADERS = mapOf("Accept" to "application/json", "Content-Type" to "application/json")
 val FILE_HEADERS = mapOf("Content-Type" to "multipart/form-data")
@@ -28,21 +29,28 @@ class DiscordLogcat(
     var webhook: String = "",
     var webhookProg: String = "",
     var webhookSpam: String = "",
-    var imgGen: String = "",
+    var progGenUrl: String = "",
+    var imgGenUrl: String = "",
 ) {
     init {
         adbPath = this::class.java.getResource("adb.properties").readText() // location of adb.exe
         webhook = this::class.java.getResource("discord.webhook").readText() // discord webhook url
         webhookProg = this::class.java.getResource("discord.webhook.prog").readText() // discord webhook url
         webhookSpam = this::class.java.getResource("discord.webhook.spam").readText() // discord webhook url
-        imgGen = this::class.java.getResource("img.gen").readText() // dank image generator
+        progGenUrl = this::class.java.getResource("prog.gen").readText() // dank image generator
+        imgGenUrl = this::class.java.getResource("img.gen").readText() // dank image generator
     }
 
-    fun getImage(payload: JSONObject): File? = try {
-        val img = khttp.post(imgGen, json = payload, headers = JSON_HEADERS)
-        val base64Img = img.jsonObject.getString("body")
-            .replace("data:image/png;base64,", "")
-            .replace("\"", "")
+    fun getImage(payload: JSONObject, url: String): File? = try {
+        val img = khttp.post(url, json = payload, headers = JSON_HEADERS)
+        val base64Img =
+            try {
+                img.jsonObject.getString("body")
+            } catch (t: Throwable) {
+                img.jsonObject.getString("img")
+            }
+                .replace("data:image/png;base64,", "")
+                .replace("\"", "")
 
         File("temp.png").apply { writeBytes(Base64.getDecoder().decode(base64Img)) }
     } catch (t: Throwable) {
@@ -74,7 +82,7 @@ class DiscordLogcat(
                             it.contains(PROG_PREFIX) -> {
                                 val jsonObject = it.sanitize().let { JSONObject(it) }
                                 println("$PREFIX $it")
-                                val img = getImage(jsonObject)
+                                val img = getImage(jsonObject, progGenUrl)
 
                                 img?.postToWebhook(webhookProg)
                                     ?: jsonObject
@@ -89,8 +97,20 @@ class DiscordLogcat(
                                     .postToWebhook(webhookSpam)
                             }
 
+                            it.contains(COLLECTION_LOG_PREFIX) -> {
+                                val jsonObject = it.sanitize().let { JSONObject(it) }
+                                println("$PREFIX $it")
+                                val img = getImage(jsonObject, imgGenUrl)
+
+                                img?.postToWebhook()
+                                    ?: jsonObject
+                                        .toString(4)
+                                        .let { "```\n$it\n```" }
+                                        .postToWebhook()
+                            }
+
                             else -> {
-                                if (it.contains("System.out")) println(it)
+                                if (it.isSysOut()) println(it.sanitizeNormalLogMessage())
                             }
                         }
                     }
@@ -106,8 +126,8 @@ class DiscordLogcat(
         println("webhook post failed ${t.stackTraceToString()}")
     }
 
-    fun File.postToWebhook(wh: String = webhook) = try {
-        khttp.post(wh, files = listOf(this.fileLike()), headers = FILE_HEADERS)
+    fun File.postToWebhook(wh: String = webhook, text: String? = "") = try {
+        khttp.post(wh, files = listOf(this.fileLike()), data = mapOf("content" to text), headers = FILE_HEADERS)
     } catch (t: Throwable) {
         println("webhook post failed ${t.stackTraceToString()}")
     }
@@ -122,6 +142,14 @@ private fun String.sanitize() =
         .replace(PROG_PREFIX, "")
         .replaceBefore(SPAM_PREFIX, "")
         .replace(SPAM_PREFIX, "")
+        .replaceBefore(COLLECTION_LOG_PREFIX, "")
+        .replace(COLLECTION_LOG_PREFIX, "")
+
+private fun String.isSysOut() = listOf("INFO", "DEBUG", "WARN", "ERROR").any { this.contains(it) }
+
+private fun String.sanitizeNormalLogMessage() =
+    String(this.toByteArray(), Charsets.US_ASCII)
+        .replace(".*System.out: ".toRegex(), "")
 
 
 
